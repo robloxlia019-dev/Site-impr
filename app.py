f932b1ccb8868933c5a0761c7ba4545040adeb73
robloxlia019-dev <robloxlia019@gmail.com>
2026-02-17 22:21:42 -0300

    Move app.run to main guard for better practice

--- app.py
+++ app.py
@@ -41,5 +41,6 @@ def stop():
 def ping():
     return "alive"
 
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
